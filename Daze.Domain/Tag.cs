75dba3d24fa2de261b5f049f5f8247568ef5b6ca
hermesxgjini <hermesxgjini@gmail.com>
2016-11-12 19:27:13 +0100

    project setup

---
+++ Daze.Domain/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Daze.Domain
+{
+    public class Tag
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ Daze.Domain/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Daze.Domain
+{
+    public class User
+    {
+        public Guid ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+}

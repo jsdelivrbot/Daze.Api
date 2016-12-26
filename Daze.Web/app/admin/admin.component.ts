c1777bc894dcf90bf6f0491f268fe50eee97a02d
hermesxgjini <hermesxgjini@gmail.com>
2016-12-26 22:03:29 +0100

    init admin area

---
+++ Daze.Web/app/admin/admin.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../shared/services/posts.service';
+import IPost = Daze.Interfaces.IPost;
+
+@Component({
+    selector: 'admin',
+    providers: [PostsService],
+    styleUrls: ['app/admin/admin.style.css'],
+    templateUrl: 'app/admin/admin.template.html'
+})
+export class AdminComponent implements OnInit {
+    private _posts = new Array<IPost>();
+    private _selectedPost: IPost = null;
+    private _isPostSelected = false;
+    private _isLoading = true;
+    constructor(private _postsService: PostsService) { }
+
+    onPostClick(id: number) {
+        this._selectedPost = this._posts.find(p => p.id == id);
+        this._isPostSelected = !this._isPostSelected;
+    }
+
+    ngOnInit() {
+        this._postsService.getPosts()
+            .subscribe(
+            post => this._posts.push(post),
+            _ => _,
+            () => this._isLoading = false);
+    }
+}

05fa344ec5118bc0c59e61e15a04f8526c70eb71
hermesxgjini <hermesxgjini@gmail.com>
2016-12-28 22:13:22 +0100

    added pixi.js

--- Daze.Web/app/shared/notfound/notfound.component.ts
+++ Daze.Web/app/shared/notfound/notfound.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'notFound',
+    styleUrls: ['app/shared/notfound/notfound.style.css'],
     template: `
-        <h2> page not found </h2>
+        <h2 class="bold"> page not found </h2>
     `
 })
 export class NotFoundComponent { }

23bb19075db40cf2ec1bfe3ce03c8366d84af040
hermesxgjini <hermesxgjini@gmail.com>
2016-12-31 23:12:18 +0100

    projects init

--- Daze.Web/app/dashboard/projects/projects.component.ts
+++ Daze.Web/app/dashboard/projects/projects.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from './projects.service';
+import IProject = Daze.Interfaces.IProject;
 
 @Component({
     selector: 'projects',
+    providers: [ProjectsService],
     styleUrls: ['app/dashboard/projects/projects.style.css'],
     templateUrl: 'app/dashboard/projects/projects.template.html'
 })
 export class ProjectsComponent implements OnInit {
-    constructor() { }
-    ngOnInit() { }
+    private _projects = new Array<IProject>();
+    constructor(private _projectsService: ProjectsService) { }
+
+    ngOnInit() {
+        this._projectsService.getData()
+            .subscribe(project => this._projects.push(project));
+    }
 }
---
+++ Daze.Web/app/shared/interfaces/project.interface.ts
@@ -0,0 +1,8 @@
+namespace Daze.Interfaces {
+
+    export interface IProject {
+        name: string;
+        description: string;
+        url: string;
+    }
+}

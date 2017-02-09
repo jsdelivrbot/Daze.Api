559b1cd89e7fb90da96f482d875aa9bb3cc4484b
hermesxgjini <hermesxgjini@gmail.com>
2017-02-09 23:37:43 +0100

    fixed configuration on suave api

--- Api.v2/Daze.Api/Daze.Api/Services.fs
+++ Api.v2/Daze.Api/Daze.Api/Services.fs
@@ -2,15 +2,19 @@
 module Daze.Api.Services
 
 open System
+open FSharp.Data
 open FSharp.Data.Sql
 open Npgsql
 open Suave
 
 let [<Literal>] DbVendor = Common.DatabaseProviderTypes.POSTGRESQL
-let [<Literal>] ConnectionString = "Host=localhost;Port=5432;Pooling=false;Database=daze_api;Username=daze;Password=daze"
+let [<Literal>] ConnectionString = "Host=localhost;Port=5432;Database=dvdrental;Username=daze;Password=daze"
 let [<Literal>] IndividualsAmount = 1000
 let [<Literal>] UseOptionTypes = false
-let [<Literal>] ResolutionPath = @"C:\Users\daze\Desktop\Daze.Api\packages\Npgsql\lib\net451\Npgsql.dll"
+// let [<Literal>] ResolutionPath = @"C:\Users\daze\Desktop\Daze.Api\packages\Npgsql\lib\net451\Npgsql.dll"
+let [<Literal>] ResolutionPath = @"C:\Users\daze\Documents\Visual Studio 2015\Projects\Daze\Api.v2\Daze.Api\packages\Npgsql\lib\net451\Npgsql.dll"
+
+let [<Literal>] Owner = "daze"
 
 type PgProvider =
     SqlDataProvider<
@@ -23,16 +27,18 @@ type PgProvider =
 
 let ctx = PgProvider.GetDataContext()
 
+
 type Post = {
     Id: Guid
     Data: string }
 
 let getPosts () =
-    let posts = query { for p in ctx.Public.MtDocPost do
-                        take 100 
-                        select { Id = p.Id ; Data = p.Data } }
-                        |> Seq.toArray
-                        |> Seq.map (fun x -> x.Data )
-                        |> Seq.reduce (fun acc curr -> acc + curr + Environment.NewLine)
-                        |> Json.toJson 
-    posts
+    []
+    // let posts = query { for p in ctx.Domain.MtDocPost do
+    //                     take 100 
+    //                     select { Id = p.Id ; Data = p.Data } }
+    //                     |> Seq.toArray
+    //                     |> Seq.map (fun x -> x.Data )
+    //                     |> Seq.reduce (fun acc curr -> acc + curr + Environment.NewLine)
+    //                     |> Json.toJson 
+    // posts

b5618fb30dc89402d3ed5342649aebfa7df25859
hermesxgjini <hermesxgjini@gmail.com>
2017-04-16 18:09:48 +0200

    please work

--- Daze.Api/Daze.Api.Services/BaseService.fs
+++ Daze.Api/Daze.Api.Services/BaseService.fs
@@ -12,8 +12,8 @@ let [<Literal>] ConnectionString = "host=35.187.43.156;database=daze_db;password
 let [<Literal>] ConnectionNameString = ""
 let [<Literal>] IndividualsAmount = 1000
 let [<Literal>] UseOptionTypes = true
-//let [<Literal>] ResolutionPath = __SOURCE_DIRECTORY__ + @"/../packages/Npgsql.3.1.10/lib/netstandard1.3/Npgsql.dll"
-let [<Literal>] ResolutionPath = __SOURCE_DIRECTORY__ + @"/../packages/Npgsql.3.1.10/lib/netstandard1.3/"
+let [<Literal>] ResolutionPath = @"..\packages\Npgsql.3.1.10\lib\netstandard1.3\Npgsql.dll"
+//let [<Literal>] ResolutionPath = __SOURCE_DIRECTORY__ + @"/../packages/Npgsql.3.1.10/lib/netstandard1.3/"
 // let [<Literal>] ResolutionPath = (Path.Combine(__SOURCE_DIRECTORY__, @"..\packages\Npgsql\lib\net451\Npgsql.dll"))
 
 type PgProvider = SqlDataProvider<

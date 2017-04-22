53a2d62e03f72ca510a07c5df12645c5bd9536f5
hermesxgjini <hermesxgjini@gmail.com>
2017-04-22 23:01:49 +0200

    init unit tests for suave apis

---
+++ Daze.Api/Daze.Api.Tests/TestServices.fs
@@ -0,0 +1,11 @@
+﻿module TestServices 
+
+open Xunit
+open Daze.Api
+
+[<Fact>]
+let ``getAllPosts returns a list of posts`` () = 
+    let posts = PostService.getAllPosts()
+    Assert.NotEmpty(posts)
+
+

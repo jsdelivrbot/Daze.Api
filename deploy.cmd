89f5a6b062efe9881cbb948a0159bf29cd66534e
afractal <hermesxgjini@gmail.com>
2017-06-27 13:02:12 +0200

    azure deployment -n

--- deploy.cmd
+++ deploy.cmd
@@ -1,13 +1,16 @@
 @echo off
+
 .paket\paket.bootstrapper.exe
 if errorlevel 1 (
   exit /b %errorlevel%
-)s
+)
+
 if not exist paket.lock (
   .paket\paket.exe install
 ) else (
   .paket\paket.exe restore
 )
+
 if errorlevel 1 (
   exit /b %errorlevel%
 )

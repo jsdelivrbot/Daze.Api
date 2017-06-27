892e7495ca14f24794d8dfb8da595f82b04500d4
afractal <hermesxgjini@gmail.com>
2017-06-27 14:08:53 +0200

    azure deployment -w

--- deploy.cmd
+++ deploy.cmd
@@ -1,46 +1,46 @@
-REM @echo off
-
-REM .paket\paket.bootstrapper.exe
-REM if errorlevel 1 (
-REM   exit /b %errorlevel%
-REM )
-
-REM if not exist paket.lock (
-REM   .paket\paket.exe install
-REM ) else (
-REM   .paket\paket.exe restore
-REM )
-
-REM if errorlevel 1 (
-REM   exit /b %errorlevel%
-REM )
-REM packages\FAKE\tools\FAKE.exe %* --fsiargs build.fsx
-
 @echo off
-cls
 
-if not exist .paket (
-  @echo "Installing Paket"
-  mkdir .paket
-  curl https://github.com/fsprojects/Paket/releases/download/1.4.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
-
-  .paket\paket.bootstrapper.exe prerelease
-  if errorlevel 1 (
-    exit /b %errorlevel%
-  )
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
 )
 
 if not exist paket.lock (
-  @echo "Installing dependencies"
   .paket\paket.exe install
 ) else (
-  @echo "Restoring dependencies"
   .paket\paket.exe restore
 )
 
 if errorlevel 1 (
   exit /b %errorlevel%
 )
+packages\FAKE\tools\FAKE.exe %* --fsiargs build.fsx
 
-@echo "Copying files to web root"
-xcopy /s /y . d:\home\site\wwwroot\
+REM @echo off
+REM cls
+
+REM if not exist .paket (
+REM   @echo "Installing Paket"
+REM   mkdir .paket
+REM   curl https://github.com/fsprojects/Paket/releases/download/1.4.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
+
+REM   .paket\paket.bootstrapper.exe prerelease
+REM   if errorlevel 1 (
+REM     exit /b %errorlevel%
+REM   )
+REM )
+
+REM if not exist paket.lock (
+REM   @echo "Installing dependencies"
+REM   .paket\paket.exe install
+REM ) else (
+REM   @echo "Restoring dependencies"
+REM   .paket\paket.exe restore
+REM )
+
+REM if errorlevel 1 (
+REM   exit /b %errorlevel%
+REM )
+
+REM @echo "Copying files to web root"
+REM xcopy /s /y . d:\home\site\wwwroot\

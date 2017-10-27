cbe7da643d21708c5c33edd263268712f26f8c53
afractal <hermesxgjini@gmail.com>
2017-10-27 21:04:56 +0200

    add app.config

--- src/persistance/db.fs
+++ src/persistance/db.fs
@@ -3,33 +3,35 @@ namespace Persistance.Db
 module BaseDb =
 
     open System
+    open System.Configuration
     open System.IO
     open FSharp.Data.Sql
+    open FSharp.Configuration
     open Npgsql
     open Domain
 
 
-    let [<Literal>] ConnectionStringHeroku = "SSL Mode=Prefer;Trust Server Certificate=true;host=ec2-79-125-13-42.eu-west-1.compute.amazonaws.com;database=d581ac58mau63r;password=314ce477224861d0f7946cd6e4f3af114cd1d45c874eab6e34602e2aafa47fd9;username=yyvtjjkjittkdg;port=5432;"
-
-    // let [<Literal>] ConnectionStringLocal = "host=localhost;database=daze_db;password=daze;username=daze;"
-    // let [<Literal>] ConnectionStringGCloud = "host=35.187.43.156;database=daze_db;password=swords$online$99;username=daze;pooling=True;"
-
     let [<Literal>] DbVendor = Common.DatabaseProviderTypes.POSTGRESQL
-    let [<Literal>] ConnectionNameString = ""
+    let [<Literal>] CompileTimeConnectionNameString = "Heroku"
     let [<Literal>] IndividualsAmount = 1000
     let [<Literal>] UseOptionTypes = true
     let [<Literal>] ResolutionPath = @"..\packages\Npgsql\lib\netstandard1.3\Npgsql.dll"
 
+
+    type Settings = AppSettings<"App.config">
+
     type PgProvider =
         SqlDataProvider<
-            DbVendor,
-            ConnectionStringHeroku,
-            ConnectionNameString,
-            ResolutionPath,
-            IndividualsAmount,
-            UseOptionTypes>
+            DatabaseVendor = DbVendor,
+            // ConnectionString = ConnectionStringHeroku,
+            ConnectionStringName = CompileTimeConnectionNameString,
+            ResolutionPath = ResolutionPath,
+            IndividualsAmount = IndividualsAmount,
+            UseOptionTypes = UseOptionTypes>
 
-    let ctx = PgProvider.GetDataContext()
+    let runtimeConnectionString = Settings.ConnectionStrings.Heroku
+
+    let ctx = PgProvider.GetDataContext runtimeConnectionString
 
     let PostContext = ctx.Public.Post
     let TagContext = ctx.Public.Tag

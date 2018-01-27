389b2937f55a2e804cadadf3a3cfa9b479acc1b5
afractal <hermesxgjini@gmail.com>
2018-01-27 10:42:42 +0100

    add proper conn string

--- src/persistance/db.fs
+++ src/persistance/db.fs
@@ -24,7 +24,7 @@ module BaseDb =
         SqlDataProvider<
             DatabaseVendor = DbVendor,
             // ConnectionString = ConnectionStringHeroku,
-            // ConnectionStringName = CompileTimeConnectionNameString,
+            ConnectionStringName = CompileTimeConnectionNameString,
             ResolutionPath = ResolutionPath,
             IndividualsAmount = IndividualsAmount,
             UseOptionTypes = UseOptionTypes>

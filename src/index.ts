a044b61ed30bfaf4807bd9e56af7e1617cb8a34a
afractal <hermesxgjini@gmail.com>
2018-03-11 20:47:48 +0100

    add webpack

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import * as express from 'express';
 import { defaultCors } from './apiConfig';
 import { pool } from './persistance/pgPool';
 import { ENV } from './common/enviromnent';
+import { json, urlencoded } from 'body-parser';
+
+// import * as  swaggerUi from 'swagger-ui-express';
+// const swaggerDocument = require('./swagger.json');
 
 const PORT = +(process.env.PORT || '8080');
 const HOST_NAME = ENV == 'production' ? '0.0.0.0' : '127.0.0.1';
@@ -11,6 +15,14 @@ const router = express.Router();
 
 router.use(defaultCors);
 router.options('*', defaultCors);
+
+router.route('/posts');
+
+app.use(urlencoded({ extended: true }));
+app.use(json);
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', router);
 
 app.all('*', (req, res) => {
     return res.end('NOT FOUND');

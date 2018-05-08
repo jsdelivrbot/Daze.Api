cad30180086b0350d8a3e9119c52cec8020f5d16
afractal <hermesxgjini@gmail.com>
2018-05-08 17:04:07 +0200

    search result_type options

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import bodyParser from "body-parser";
 import expressValidator from "express-validator";
 import compression from "compression";
 import mountRoutes from "./routers";
+// @ts-ignore
+import { version } from '../package.json';
 
 const PORT = process.env.PORT || '8080';
 const HOST_NAME = ENV == 'production' ? '0.0.0.0' : '127.0.0.1';
@@ -25,7 +27,12 @@ app.use(expressValidator());
 // route handlers
 mountRoutes(app);
 
-app.get('/', (req, res) => res.end('__daze_api__'));
-app.all('*', (req, res) => res.status(404).end('NOT FOUND'));
+app.get('/', (req, res) => {
+    res.end(`__daze_api__ v${version}`);
+});
+
+app.all('*', (req, res) => {
+    res.status(404).end('NOT FOUND');
+});
 
 export default app;

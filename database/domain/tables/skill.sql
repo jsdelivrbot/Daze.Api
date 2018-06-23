a0ab2449fd1afda9a87a267c36486bed93246604
afractal <hermesxgjini@gmail.com>
2018-06-23 21:30:25 +0200

    update skill table definition

--- database/domain/tables/skill.sql
+++ database/domain/tables/skill.sql
@@ -2,10 +2,10 @@ set search_path = public;
 
 create table if not exists skill (
     id bigint primary key not null default public.id_generator(),
-	name varchar(200),
+    name varchar(200),
     level int default 0,
-	focus_area varchar(200)
-    --focus_area focus_area_type
+    -- focus_area varchar(200)
+    -- focus_area focus_area_type
 );
 
 -- (* ends here *)
